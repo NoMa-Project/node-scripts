b550956342944b12c30a98cb5e3ef176db2ffe25
MazBaz <86683229+MazBazDev@users.noreply.github.com>
2023-06-11 16:41:26 +0200

    Update install_wordpress.sh

--- install_wordpress.sh
+++ install_wordpress.sh
@@ -1,111 +1,102 @@
 #!/bin/bash
-# This script can create a WordPress website (including database setup, linking, and HTTPS)
-# Parameters: sitename, db_name, db_user, db_password, localhost (if db is not on localhost)
+#this script creates n number of website (wordpress + database + link both + https)
+#param : sitename, db_name, db_user, db_password, localhost (if db is not on localhost)
 
-set -e
-
-# Update and upgrade system
-apt update && apt upgrade -y
-
-# Install required packages
+apt update 
+apt upgrade -y
 apt install apache2 ghostscript libapache2-mod-php mysql-server php php-bcmath php-curl php-imagick php-intl php-json php-mbstring php-mysql php-xml php-zip -y
+data=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
 
-# Install WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+sudo ufw allow "Apache Full"
+sudo a2enmod ssl
+sudo systemctl restart apache2
 
-# Enable firewall
+#get necessary informations from user
+echo "Sitename cannot be wordpress."
+read -p "Entrez le nom de votre site: " sitename
+read -p "Entrez le nom de la base de données: " db_name
+read -p "Entrez le nom d'utilisateur de la base de données: " db_user
+read -s -p "Entrez le mot de passe de la base de données: " db_password
+
+#create ssl key and certificate
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/$sitename.key -out /etc/ssl/certs/$sitename.crt
+#enter ip when asked common name or sitename.com
+
+echo "<VirtualHost *:443>" >> /etc/apache2/sites-available/$sitename.conf
+echo "   ServerName $sitename.com" >> /etc/apache2/sites-available/$sitename.conf
+echo "   DocumentRoot /var/www/$sitename" >> /etc/apache2/sites-available/$sitename.conf
+
+echo "   SSLEngine on" >> /etc/apache2/sites-available/$sitename.conf
+echo "   SSLCertificateFile /etc/ssl/certs/$sitename.crt" >> /etc/apache2/sites-available/$sitename.conf
+echo "   SSLCertificateKeyFile /etc/ssl/private/$sitename.key" >> /etc/apache2/sites-available/$sitename.conf
+echo "</VirtualHost>" >> /etc/apache2/sites-available/$sitename.conf
+
+#download and install wordpress
+wget https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+mv wordpress /var/www/$sitename
+rm -rf latest.tar.gz
+chown -R www-data:www-data /var/www/wordpress
+cp -r /var/www/wordpress /var/www/$sitename
+rm -rf /var/www/wordpress
+sudo chmod -R 755 /var/www/$sitename
+
+sudo a2ensite $sitename.conf
+sudo apache2ctl configtest
+sudo systemctl reload apache2
+sudo ufw allow "Apache Full"
+
+#configuration of apache for wordpress
+echo "<VirtualHost *:80>
+  ServerName $sitename.com
+  ServerAlias www.$sitename.com/
+  DocumentRoot /var/www/$sitename/
+<Directory /var/www/$sitename>
+  Options FollowSymLinks
+  AllowOverride Limit Options FileInfo
+  DirectoryIndex index.php
+  Require all granted
+</Directory>
+<Directory /var/www/$sitename/wp-content>
+  Options FollowSymLinks
+  Require all granted
+</Directory>
+</VirtualHost>" >> /etc/apache2/sites-available/$sitename.conf
+
+#activate new site
+a2ensite $sitename
+
+#config hosts file
+myip=$(ip a s dev ens33 | awk '/inet /{print $2}' | cut -d/ -f1)
+echo "$myip $sitename.com" >> /etc/hosts
+a2dissite 000-default
+systemctl restart apache2
 ufw allow "Apache Full"
 
-# Enable SSL
-a2enmod ssl
-systemctl restart apache2
-
-# Get necessary information from user
-echo "Sitename cannot be 'wordpress'."
-read -p "Enter the name of your site: " sitename
-read -p "Enter the domain name: " fqdn
-read -p "Enter the database name: " db_name
-read -p "Enter the database username: " db_user
-read -s -p "Enter the database password: " db_password
-echo
-
-# Create SSL key and certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/$sitename.key -out /etc/ssl/certs/$sitename.crt
-
-# Create Apache virtual host configuration
-cat << EOF > /etc/apache2/sites-available/$sitename.conf
-<VirtualHost *:80>
-    ServerName $fqdn
-    ServerAlias www.$fqdn
-    DocumentRoot /var/www/$sitename/
-
-    <Directory /var/www/$sitename/>
-        Options FollowSymLinks
-        AllowOverride Limit
-        DirectoryIndex index.php
-        Require all granted
-    </Directory>
-
-    <Directory /var/www/$sitename/wp-content>
-        Options FollowSymLinks
-        Require all granted
-    </Directory>
-</VirtualHost>
-
-<VirtualHost *:443>
-    ServerName $fqdn
-    DocumentRoot /var/www/$sitename/
-    SSLEngine on
-    SSLCertificateFile /etc/ssl/certs/$sitename.crt
-    SSLCertificateKeyFile /etc/ssl/private/$sitename.key
-
-    <Directory /var/www/$sitename/>
-        Options FollowSymLinks
-        AllowOverride Limit
-        DirectoryIndex index.php
-        Require all granted
-    </Directory>
-
-    <Directory /var/www/$sitename/wp-content>
-        Options FollowSymLinks
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
-
-# Enable the site
-a2ensite $sitename.conf
-
-# Update hosts file
-myip=$(ip a s dev ens33 | awk '/inet /{print $2}' | cut -d/ -f1)
-echo "$myip $fqdn" >> /etc/hosts
-
-# Restart Apache
-systemctl restart apache2
-
-# Create database and user
+#configuration of database for wordpress
 mysql -u root -p <<MYSQL_SCRIPT
-CREATE DATABASE IF NOT EXISTS $db_name;
-CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_password';
-ALTER USER '$db_user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';
+CREATE DATABASE $db_name;
+CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_password';
 GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
-# Download and install WordPress
-mkdir /var/www/$sitename/ 
-cd /var/www/$sitename/
-wp core download --allow-root
-wp config create --dbname=$db_name --dbuser=$db_user --dbpass=$db_password --dbhost=localhost --skip-check --allow-root
+#create wordpress configuration file
+echo "<?php" >> /var/www/$sitename/wp-config.php
 
-# Auto-install WordPress
-wp core install --url=$fqdn --title="$sitename" --admin_user=admin --admin_password=admin_password --admin_email=admin@$fqdn --skip-email --allow-root
+#configuration of wordpress to use this database
+echo "define( 'DB_NAME', '$db_name' );
+define( 'DB_USER', '$db_user' );
+define( 'DB_PASSWORD', '$db_password' );" >> /var/www/$sitename/wp-config.php
+#attention : remplacer localhost par l'ip de la machine qui heberge la db
+echo "define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+$data
+\$table_prefix = 'wp_';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) { define( 'ABSPATH', __DIR__ . '/' ); }
+require_once ABSPATH . 'wp-settings.php';
+?>" >> /var/www/$sitename/wp-config.php
 
-# Set ownership and permissions
-chown -R www-data:www-data /var/www/$sitename/
-chmod -R 755 /var/www/$sitename/
-
-echo "The website $fqdn has been successfully created!"
-echo "Wordpress Credentials = User : admin  Password : admin_password Email : admin@$fqnd"
+echo "Le site web $sitename.com a été créé avec succès !"
